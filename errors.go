0780a96d440fe9ce5773612bd8f8ccfa3fc799fa
Hanes Oliveira <hanesoliveira@98247598273489.local>
2023-03-01 12:08:13 -0700

    Refactoring.

--- errors.go
+++ errors.go
@@ -2,15 +2,20 @@ package sim2d
 
 import "errors"
 
+// b is the base.
+// q is higher power.
+// p is the lower power.
+// d is the degree.
+
 var (
-	ErrDIsLessThanOrEqualToQPlusP            = errors.New("degree should be greater than q plus the absolute value of p")
+	ErrDIsLessThanOrEqualToQPlusP            = errors.New("degree should be greater than the higher power plus the absolute value of the lower power")
 	ErrDIsLessThanOne                        = errors.New("degree should be greater than or equal to 1")
 	ErrDIsNotAPowerOf2                       = errors.New("degree should be a power of 2")
-	ErrPIsLessThanQ                          = errors.New("p should be less than q")
-	ErrPIsGreaterThanOrEqualToZero           = errors.New("p should be less than 0")
-	ErrQIsLessThanOrEqualToZero              = errors.New("q is greater than 0")
+	ErrPIsLessThanQ                          = errors.New("the lower power should be less than the higher power")
+	ErrPIsGreaterThanOrEqualToZero           = errors.New("the lower power should be less than 0")
+	ErrQIsLessThanOrEqualToZero              = errors.New("higher power is greater than 0")
 	ErrBIsLessThan2                          = errors.New("base should be greater than or equal to 2")
-	ErrDenominatorIsNotEqualToBToThePowerOfP = errors.New("denominator should be the base to the power of the absolute value of p")
+	ErrDenominatorIsNotEqualToBToThePowerOfP = errors.New("denominator should be the base to the power of the absolute value of the lower power")
 	ErrNumeratorIsNotInTheMessageSpaceRange  = errors.New("numerator should be inside the message space range")
 	ErrCodeDegreeIsNotAPowerOf2              = errors.New("code degree should be a power of 2")
 )
