e3c4a5077752f4b4acfb29cb0be4f362afc970ad
Hanes Oliveira <hanesoliveira@protonmail.com>
2023-08-14 12:47:57 -0600

    Refactoring.

--- validate.go
+++ validate.go
@@ -7,16 +7,16 @@ import (
 // inputIsInvalid checks if the number given to the function is in the input space.
 // We just consider the case when the parity of the base is even.
 func inputIsInvalid(input int64, params *Parameters) bool {
-	// Define a common component of all bounds: b^(q-p+1) - 1.
-	b, q, p := float64(params.Base()), params.GreatPower(), params.LeastPower()
-	// Exponent: e = q-p+1.
-	e := float64(q - p + 1)
-	// b^(q-p+1) - 1
+	// Define a common component of all bounds: b^(gp-lp+1) - 1.
+	b, gp, lp := float64(params.Base()), params.GreatPower(), params.LeastPower()
+	// Exponent: e = gp-lp+1.
+	e := float64(gp - lp + 1)
+	// b^(gp-lp+1) - 1
 	bp := math.Pow(b, e) - 1
 	// We define the lower and upper bounds by defining the equations in separated parts.
-	// Lower bound: -b/2 x (b^(q-p+1) - 1) / (b-1).
+	// Lower bound: -b/2 x (b^(gp-lp+1) - 1) / (b-1).
 	lb := -b / 2 * bp / (b - 1)
-	// Upper bound: (b/2 - 1) x ((b^(q-p+1) - 1) / (b-1)).
+	// Upper bound: (b/2 - 1) x ((b^(gp-lp+1) - 1) / (b-1)).
 	ub := (b/2 - 1) * bp / (b - 1)
 	// Check if number is less than lower bound or greater than upper bound.
 	return input < int64(lb) || int64(ub) < input
